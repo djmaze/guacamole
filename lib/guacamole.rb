2c970953f74c8f65cc2b5b6885d6d2882a04b0b6
Dirk Breuer <dirk.breuer@gmail.com>
2013-10-19 12:50:20 +0200

    Add a Railtie stub

--- lib/guacamole.rb
+++ lib/guacamole.rb
@@ -1,11 +1,16 @@
 # -*- encoding : utf-8 -*-
+
+require 'active_support/core_ext'
+
 require 'guacamole/version'
 require 'guacamole/configuration'
 require 'guacamole/model'
 require 'guacamole/collection'
 require 'guacamole/document_model_mapper'
 
-require 'active_support/core_ext'
+if defined?(Rails)
+  require 'guacamole/railtie'
+end
 
 module Guacamole
 end
---
+++ lib/guacamole/railtie.rb
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+require 'rails'
+
+module Rails
+  module Guacamole
+    class Railtie < Rails::Railtie
+    end
+  end
+end
